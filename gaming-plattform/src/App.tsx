7525cf2e39eaabc5ea62b46b43b4027b2e25b461
kiliantrauner92 <kilian.trauner@gmail.com>
2024-04-10 22:08:32 +0200

    Fetching data with axios.

--- gaming-plattform/src/App.tsx
+++ gaming-plattform/src/App.tsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
 import ExpenseBlock from "./expense-tracker/components/ExpenseBlock";
 
+interface User {
+  id: number;
+  name: string;
+}
+
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       var response = await axios.get<User[]>(
+  //         "https://jsonplaceholder.typicode.com/users"
+  //       );
+  //       if (response) {
+  //         setUsers(response.data);
+  //       }
+  //     } catch (err) {
+  //       setError((err as AxiosError).message);
+  //     }
+  //   };
+
+  //   fetchUsers();
+  // }, []);
   return (
     <>
-      <ExpenseBlock />
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
