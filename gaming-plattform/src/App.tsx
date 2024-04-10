7b7082eefa335bd421554eda12a0f23e0b065ef7
kiliantrauner92 <kilian.trauner@gmail.com>
2024-04-10 20:01:12 +0200

    Cleared App.tsx before proceeding

--- gaming-plattform/src/App.tsx
+++ gaming-plattform/src/App.tsx
@@ -1,55 +1,10 @@
 import { useState } from "react";
-
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-
-export interface Expense {
-  id: number;
-  description: string;
-  amount: number;
-  category: string;
-}
+import ExpenseBlock from "./expense-tracker/components/ExpenseBlock";
 
 function App() {
-  const [expenses, setExpenses] = useState<Expense[]>([
-    { id: 1, description: "Test", amount: 5, category: "Groceries" },
-    { id: 2, description: "Test2", amount: 3, category: "Utilities" },
-  ]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((expenses) => expenses.category === selectedCategory)
-    : expenses;
   return (
     <>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([
-              ...expenses,
-              {
-                ...expense,
-                id: expenses.findLastIndex((value) => value.id) + 1,
-              },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category: string) => setSelectedCategory(category)}
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseList
-          expenses={visibleExpenses}
-          onDelete={(id) =>
-            setExpenses(visibleExpenses.filter((expense) => expense.id !== id))
-          }
-          // onFilter={filterExpenses}
-        />
-      </div>
+      <ExpenseBlock />
     </>
   );
 }
