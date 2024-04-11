ebed1da8b491fadbe5363c35eb3b8d5a415d9c3f
kiliantrauner92 <kilian.trauner@gmail.com>
2024-04-11 22:07:56 +0200

    Added a custom hook to make user functionality reuseable.

--- gaming-plattform/src/App.tsx
+++ gaming-plattform/src/App.tsx
@@ -1,45 +1,8 @@
-import { useState, useEffect } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return cancel;
-  }, []);
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       var response = await axios.get<User[]>(
-  //         apiClient + "/users"
-  //       );
-  //       if (response) {
-  //         setUsers(response.data);
-  //       }
-  //     } catch (err) {
-  //       setError((err as AxiosError).message);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  // }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((x) => x.id !== user.id));
