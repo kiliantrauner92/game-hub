9e0b99f91d75befbd3871e1927c2d92e94a396ed
kiliantrauner92 <kilian.trauner@gmail.com>
2024-04-11 20:48:07 +0200

    Added function to update users and send a patch message.

--- gaming-plattform/src/App.tsx
+++ gaming-plattform/src/App.tsx
@@ -58,6 +58,20 @@ function App() {
         setUsers(originalUsers);
       });
   };
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" } as User;
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   const addUser = () => {
     const originalUsers = [...users];
     const newUser = { id: 0, name: "Kilian" };
@@ -86,12 +100,20 @@ function App() {
             key={user.id}
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
